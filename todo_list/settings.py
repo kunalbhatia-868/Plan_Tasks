9f0fd6475d3707c12416f2820a4e861271d24b68
kunal <kunalbhatia868@gmail.com>
2022-05-04 18:49:53 +0530

    env setup

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -10,9 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from email.policy import default
 from pathlib import Path
 import os
-
+import decouple
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,12 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q=v6jq$rr5&m#i*1zzlme73wv=dm6ioty5lw12xj8+_dse@%4f'
+SECRET_KEY = decouple.config('SECRET_KEY',default='')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = decouple.config('DEBUG',cast=bool,default=True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    host.strip()
+    for host in decouple.config('ALLOWED_HOSTS',default='').split(',')
+]
 
 
 # Application definition
