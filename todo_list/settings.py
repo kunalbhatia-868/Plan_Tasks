1c3ddc30e7146c3898a8330b93170981da1126ea
kunal <kunalbhatia868@gmail.com>
2022-05-04 19:32:04 +0530

    django-heroku auto configure

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = decouple.config('SECRET_KEY',default='')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = decouple.config('DEBUG',cast=bool,default=True)
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = [
     host.strip()
